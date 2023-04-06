fc53ec1b2c6c0449563799392acd8480ff1b34db
José Manuel Sánchez <buscarini@gmail.com>
2023-04-06 15:55:18 +0200

    feat: Added splitBy optic.

---
+++ Tests/OpticsTests/StringOpticTests.swift
@@ -0,0 +1,26 @@
+import Foundation
+import Optics
+import XCTest
+
+class StringOpticTests: XCTestCase {
+	func testSplitBy() {
+		let value = "hello world!"
+		
+		let optic = Lens {
+			\String.self
+		}.splitBy(separator: " ")
+		
+		
+		XCTAssertEqual(
+			optic.get(value),
+			["hello", "world!"]
+		)
+			
+		XCTAssertEqual(
+			optic.update(value) {
+				$0 + [ "I", "am", "Groot!" ]
+			},
+			"hello world! I am Groot!"
+		)
+	}
+}
