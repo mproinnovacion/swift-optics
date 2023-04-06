5d983929fb1ce45312f200da8e1ad9bd35cfa744
José Manuel Sánchez <buscarini@gmail.com>
2023-04-06 18:27:10 +0200

    feat: JoinedBy.

--- Tests/OpticsTests/StringOpticTests.swift
+++ Tests/OpticsTests/StringOpticTests.swift
@@ -23,4 +23,25 @@ class StringOpticTests: XCTestCase {
 			"hello world! I am Groot!"
 		)
 	}
+	
+	func testJoinedBy() {
+		let value = ["hello", "world!"]
+		
+		let optic = Lens {
+			\[String].self
+		}.joinedBy(separator: " ")
+		
+		
+		XCTAssertEqual(
+			optic.get(value),
+			"hello world!"
+		)
+			
+		XCTAssertEqual(
+			optic.update(value) {
+				$0 + " I am Groot!"
+			},
+			[ "hello", "world!", "I", "am", "Groot!" ]
+		)
+	}
 }
