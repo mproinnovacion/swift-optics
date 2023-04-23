f495734fb848b3e5a587d414da7a5c76c0fa4fde
José Manuel Sánchez <buscarini@gmail.com>
2023-04-23 15:44:12 +0200

    feat: Added FoldMap. Added monoidal implementation for fold and foldmap. Added fold and foldMap functions for ArrayGetterOptic.

--- Sources/Optics/Fold/FoldCombinators.swift
+++ Sources/Optics/Fold/FoldCombinators.swift
@@ -18,10 +18,54 @@ public struct Fold<O: ArrayGetterOptic>: GetterOptic {
 		self.monoid = monoid
 	}
 	
+	@inlinable
+	public init(
+		@FoldBuilder<Whole, Part> with build: () -> O
+	) where Part: Monoidal {
+		self.optic = build()
+		self.monoid = Part.monoid
+	}
+	
 	public func get(
 		_ whole: Whole
 	) -> Part {
 		self.optic.getAll(whole).reduced(monoid)
+	}
+}
+
+public struct FoldMap<O: ArrayGetterOptic, Result>: GetterOptic {
+	public typealias Whole = O.Whole
+	public typealias Part = Result
+	
+	public let optic: O
+	public let monoid: Monoid<Part>
+	public let map: (O.Part) -> Result
+	
+	@inlinable
+	public init(
+		monoid: Monoid<Part>,
+		map: @escaping (O.Part) -> Result,
+		@FoldBuilder<Whole, O.Part> with build: () -> O
+	) {
+		self.optic = build()
+		self.monoid = monoid
+		self.map = map
+	}
+	
+	@inlinable
+	public init(
+		map: @escaping (O.Part) -> Result,
+		@FoldBuilder<Whole, O.Part> with build: () -> O
+	) where Result: Monoidal {
+		self.optic = build()
+		self.monoid = Result.monoid
+		self.map = map
+	}
+	
+	public func get(
+		_ whole: Whole
+	) -> Part {
+		self.optic.getAll(whole).foldMap(map, monoid)
 	}
 }
 
