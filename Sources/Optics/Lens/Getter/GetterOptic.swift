05da3d25f8e3124f0a8b6c4b62bb59de2c29de6f
José Manuel Sánchez <buscarini@gmail.com>
2023-04-30 19:59:50 +0200

    feat: ThrowingGetter provide whole.

--- Sources/Optics/Lens/Getter/GetterOptic.swift
+++ Sources/Optics/Lens/Getter/GetterOptic.swift
@@ -14,15 +14,45 @@ extension GetterOptic {
 	}
 }
 
-extension GetterOptic where Whole == Void {
-	public func get() -> Part {
-		self.get(())
-	}
-}
-
 extension KeyPath: GetterOptic {
 	@inlinable
 	public func get(_ whole: Root) -> Value {
 		whole[keyPath: self]
+	}
+}
+
+public struct GetterProvidedWholeOptic<O: GetterOptic>: GetterOptic {
+	public typealias Whole = Void
+	public typealias Part = O.Part
+	
+	public let optic: O
+	public let whole: O.Whole
+	
+	public init(
+		optic: O,
+		whole: O.Whole
+	) {
+		self.optic = optic
+		self.whole = whole
+	}
+	public func get(_ whole: Void) -> O.Part {
+		optic.get(self.whole)
+	}
+}
+
+extension GetterOptic {
+	public func provide(
+		_ whole: Whole
+	) -> GetterProvidedWholeOptic<Self> {
+		.init(
+			optic: self,
+			whole: whole
+		)
+	}
+}
+
+extension GetterOptic where Whole == Void {
+	public func get() -> Part {
+		self.get(())
 	}
 }
