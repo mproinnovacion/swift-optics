c941e1582d2ca100199fd6db0708505b3a7e79b8
José Manuel Sánchez <buscarini@gmail.com>
2023-05-13 19:35:40 +0200

    test: Added test for func3 in array getter.

--- Tests/OpticsTests/ArrayGetter/ArrayGetterFuncTests.swift
+++ Tests/OpticsTests/ArrayGetter/ArrayGetterFuncTests.swift
@@ -46,4 +46,30 @@ class ArrayGetterFuncTests: XCTestCase {
 			[4, 5]
 		)
 	}
+	
+	func testFunc3() {
+		struct User {
+			var name: String
+			var surname: String
+			var nickname: String
+		}
+		
+		let getter = ManyGetter {
+			ConcatGetters {
+				\User.name
+				\User.surname
+				\User.nickname
+			}
+			{ (string: String) -> Int in
+				string.count
+			}
+		}
+		
+		let user = User(name: "john", surname: "Smith", nickname: "johnny")
+		
+		XCTAssertEqual(
+			getter.getAll(user),
+			[4, 5, 6]
+		)
+	}
 }
